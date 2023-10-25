d53f531796a72dbafc4e93e5e14aaf2e39e417ed
Melih Catal <46859098+melihcatal@users.noreply.github.com>
2023-10-25 13:13:25 +0300

    close Should list available models through CLI #65

--- cli.py
+++ cli.py
@@ -1,93 +1,41 @@
 # ignore warnings
 import warnings
-warnings.simplefilter(action='ignore', category=UserWarning)
-
-from logger_config import setup_logging
-setup_logging()
-
 import click
-from advsecurenet.attacks import lots
-from advsecurenet.models.vgg16 import VGG16Features
-import torch
-from PIL import Image
-import os
-import datetime
-import torchvision.transforms as transforms
-
-
+warnings.simplefilter(action='ignore', category=UserWarning)
+from advsecurenet.models import ModelFactory, ModelType
 @click.group()
 def main():
-    click.echo("Welcome to the Adversarial Secure Network CLI!")
-
-def determine_output_directory(command_name, output):
-    # Determine the output directory
-    if output is None:
-        output = os.path.join("outputs", command_name, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    else:
-        output = os.path.join(output, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    if not os.path.exists(output):
-        os.makedirs(output)
-
-    return output
-
-def get_device():
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    click.echo("Welcome to the Adversarial Secure Networks CLI!")
 
 @main.command()
-@click.option('--targeted', is_flag=True, help='Run a targeted attack. Defaults to False (untargeted).', default=False) 
-@click.option('--input-image-path', default=None, help='Path to the input image.')
-@click.option('--input-descriptor-path', default=None, help='Path to the serialized input descriptor.')
-@click.option('--target-image-path', default=None, help='Path to the target image.')
-@click.option('--target-descriptor-path', default=None, help='Path to the serialized target descriptor.')
-@click.option('--output', default=None, help='Custom output directory. Defaults to a timestamped folder in outputs/<command_name>.')
-def run_lots(targeted, input_image_path,input_descriptor_path, target_image_path, target_descriptor_path, output):
-    """Run LOTS to create an adversarial image."""
-    # Initialize the model
-    model = VGG16Features().eval()
-    device = get_device()
-    model.to(device)
+@click.option('--model-type', 
+              type=click.Choice([e.value for e in ModelType] + ['all']), 
+              default='all', 
+              help="The type of model to list. 'custom' for custom models, 'standard' for standard models, and 'all' for all models. Default is 'all'.")
+def models(model_type):
+    """Command to list available models."""
+    model_list = _get_models(model_type)
 
-    # Determine the output directory
-    output = determine_output_directory("run_lots", output)
+    click.echo("Available models:")
+    for model in model_list:
+        click.echo(model)
 
-    if input_image_path:
-        _, input_descriptor = model(model.preprocess(Image.open(input_image_path)).unsqueeze(0).to(device))
-    elif input_descriptor_path:
-        input_descriptor = torch.load(input_descriptor_path).to(device)
-    else:
-        print("You must provide either an input image path or an input descriptor path!")
-        return
+def _get_models(model_type: str) -> list[str]:
 
-    input_tensor = model.preprocess(Image.open(input_image_path)).unsqueeze(0).to(device)
+    model_list_getters = {
+            "all": ModelFactory.get_available_models,
+            "custom": ModelFactory.get_available_custom_models,
+            "standard": ModelFactory.get_available_standard_models
+        }
 
-    if targeted and target_image_path:
-        target_tensor = model.preprocess(Image.open(target_image_path)).unsqueeze(0).to(device)
-    elif targeted and target_descriptor_path:
-        target_tensor = torch.load(target_descriptor_path).to(device)
-    elif targeted and (not target_image_path or not target_descriptor_path):
-        print("You must provide either a target image path or a target descriptor path!")
-        return
-    elif not targeted and target_image_path:
-        print("You cannot provide a target image path for an untargeted attack!")
-        return
-    elif not targeted and target_descriptor_path:
-        print("You cannot provide a target descriptor path for an untargeted attack!")
-        return
-    else:
-        target_descriptor = torch.zeros(4096).to(device)  # Dummy target descriptor
+    model_list = model_list_getters.get(model_type, lambda: [])()
+    if not model_list:
+        raise ValueError("Unsupported model type!")
+    return model_list
 
-    adversarial, _ = lots.lots_iterative(model, input_tensor.squeeze(0), target_descriptor)
-    
-    print(f"Adversarial class for {input_image_path}: {model.predict_class(adversarial.unsqueeze(0))}")
-    # save the original class and the adversarial class to a file in format "original_class,adversarial_class"
-    with open(os.path.join(output, "original_class,adversarial_class"), "w") as f:
-        f.write(f"{model.predict_class(input_tensor)},{model.predict_class(adversarial.unsqueeze(0))}")
-    
-    # save the adversarial image
-    adversarial_image = transforms.ToPILImage()(adversarial.cpu().detach().squeeze(0))
-    adversarial_image.save(os.path.join(output, "adversarial.png"))
-    
-    
 if __name__ == "__main__":
     main()
+
+
+
+
