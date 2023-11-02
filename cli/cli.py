e0f3203fcf060eeb345c92fa10b30eeee51e5276
Melih Catal <46859098+melihcatal@users.noreply.github.com>
2023-11-02 17:02:14 +0300

    add docs to cli attacks

--- cli/cli.py
+++ cli/cli.py
@@ -308,6 +308,39 @@ def execute_general_attack(attack_type: AttackType, config_file: str, attack_con
 @click.option('--max-iterations', default=None, type=click.INT, help='Number of iterations for the attack.')
 @click.option('--overshoot', default=None, type=click.FLOAT, help='Overshoot value for the attack.')
 def deepfool(config, **kwargs):
+    """
+    Command to execute a DeepFool attack.
+
+    Args:
+
+        config (str, optional): Path to the attack configuration yml file.
+        model_name (str): The name of the model to be attacked.
+        trained_on (str): Dataset on which the model was trained.
+        model_weights (str): Path to model weights. If unspecified, uses the default path based on model_name and trained_on.
+        device (str, optional): Device for executing attacks. Defaults to CPU.
+        dataset_name (str, optional): Dataset for the attack. Choose "custom" for your own dataset.
+        custom_data_dir (str, optional): Path to custom dataset. Required if dataset_name is "custom".
+        dataset_part (str, optional): Which part of dataset to use for attack. Ignored if dataset_name is "custom".
+        random_samples (int, optional): Number of random samples for attack. Relevant only if dataset_part is "random" and dataset_name isn't "custom".
+        batch_size (int, optional): Batch size for attack execution. Defaults to 32.
+        verbose (bool, optional): Whether to print progress of the attack. Defaults to True.
+        save_result_images (bool, optional): Whether to save the adversarial images. Defaults to False.
+        result_images_dir (str, optional): Directory to save the adversarial images. Defaults to None.
+        num_classes (int, optional): Number of classes for the attack. Defaults to None.
+        max_iterations (int, optional): Number of iterations for the attack. Defaults to None.
+        overshoot (float, optional): Overshoot value for the attack. Defaults to None.
+
+    Examples:
+    
+            >>> advsecurenet attack deepfool --model-name=resnet18 --trained-on=cifar10 --model-weights=resnet18_cifar10_weights.pth
+            or
+            >>> advsecurenet attack deepfool --config=deepfool_attack_config.yml
+
+    Notes:
+    
+            If a configuration file is provided, matching CLI arguments will override the configuration file. The CLI arguments have priority.
+            Configuration file attributes must match the CLI arguments. For example, if the configuration file has a "model_name" attribute, the CLI argument must be named "model_name" as well.
+    """
     return execute_general_attack(AttackType.DEEPFOOL, config, attack_configs.DeepFoolAttackConfig, **kwargs)
 
 
@@ -326,8 +359,50 @@ def deepfool(config, **kwargs):
 @click.option('--c-lower', type=click.FLOAT, default=None, help='The lower bound for c. Defaults to 1e-6.')
 @click.option('--c-upper', type=click.FLOAT, default=None, help='The upper bound for c. Defaults to 1.')
 @click.option('--patience', type=click.INT, default=None, help='The number of iterations to wait before early stopping. Defaults to 5.')
-@click.option('--verbose', type=click.BOOL, default=None, help='Whether to print progress of the attack. Defaults to True.')
 def cw(config, **kwargs):
+    """
+    Command to execute a Carlini-Wagner attack.
+
+    Args:
+
+        config (str, optional): Path to the attack configuration yml file.
+        model_name (str): The name of the model to be attacked.
+        trained_on (str): Dataset on which the model was trained.
+        model_weights (str): Path to model weights. If unspecified, uses the default path based on model_name and trained_on.
+        device (str, optional): Device for executing attacks. Defaults to CPU.
+        dataset_name (str, optional): Dataset for the attack. Choose "custom" for your own dataset.
+        custom_data_dir (str, optional): Path to custom dataset. Required if dataset_name is "custom".
+        dataset_part (str, optional): Which part of dataset to use for attack. Ignored if dataset_name is "custom".
+        random_samples (int, optional): Number of random samples for attack. Relevant only if dataset_part is "random" and dataset_name isn't "custom".
+        batch_size (int, optional): Batch size for attack execution. Defaults to 32.
+        verbose (bool, optional): Whether to print progress of the attack. Defaults to True.
+        save_result_images (bool, optional): Whether to save the adversarial images. Defaults to False.
+        result_images_dir (str, optional): Directory to save the adversarial images. Defaults to None.
+        targeted (bool, optional): Whether to perform a targeted attack. Defaults to False.
+        c_init (float, optional): The initial value of c to use for the attack. Defaults to 0.1.
+        kappa (float, optional): The confidence value to use for the attack. Defaults to 0.
+        learning_rate (float, optional): The learning rate to use for the attack. Defaults to 0.01.
+        max_iterations (int, optional): The maximum number of iterations to use for the attack. Defaults to 10.
+        abort_early (bool, optional): Whether to abort the attack early if the loss stops decreasing. Defaults to False.
+        binary_search_steps (int, optional): The number of binary search steps to use for the attack. Defaults to 10.
+        clip_min (float, optional): The minimum value for clipping pixel values. Defaults to 0.
+        clip_max (float, optional): The maximum value for clipping pixel values. Defaults to 1.
+        c_lower (float, optional): The lower bound for c. Defaults to 1e-6.
+        c_upper (float, optional): The upper bound for c. Defaults to 1.
+        patience (int, optional): The number of iterations to wait before early stopping. Defaults to 5.
+
+    Examples:
+    
+            >>> advsecurenet attack cw --model-name=resnet18 --trained-on=cifar10 --model-weights=resnet18_cifar10_weights.pth
+            or
+            >>> advsecurenet attack cw --config=cw_attack_config.yml
+
+    Notes:
+
+            If a configuration file is provided, matching CLI arguments will override the configuration file. The CLI arguments have priority.
+            Configuration file attributes must match the CLI arguments. For example, if the configuration file has a "model_name" attribute, the CLI argument must be named "model_name" as well.
+
+    """
     return execute_general_attack(AttackType.CW, config, attack_configs.CWAttackConfig, **kwargs)
 
 
@@ -337,6 +412,38 @@ def cw(config, **kwargs):
 @click.option('--num-iter', default=None, type=click.INT, help='Number of iterations for the attack.')
 @click.option('--alpha', default=None, type=click.FLOAT, help='Alpha value for the attack.')
 def pgd(config, **kwargs):
+    """
+    Command to execute a PGD attack.
+
+    Args:
+        config (str, optional): Path to the attack configuration yml file.
+        model_name (str): The name of the model to be attacked.
+        trained_on (str): Dataset on which the model was trained.
+        model_weights (str): Path to model weights. If unspecified, uses the default path based on model_name and trained_on.
+        device (str, optional): Device for executing attacks. Defaults to CPU.
+        dataset_name (str, optional): Dataset for the attack. Choose "custom" for your own dataset.
+        custom_data_dir (str, optional): Path to custom dataset. Required if dataset_name is "custom".
+        dataset_part (str, optional): Which part of dataset to use for attack. Ignored if dataset_name is "custom".
+        random_samples (int, optional): Number of random samples for attack. Relevant only if dataset_part is "random" and dataset_name isn't "custom".
+        batch_size (int, optional): Batch size for attack execution. Defaults to 32.
+        verbose (bool, optional): Whether to print progress of the attack. Defaults to True.
+        save_result_images (bool, optional): Whether to save the adversarial images. Defaults to False.
+        result_images_dir (str, optional): Directory to save the adversarial images. Defaults to None.
+        epsilon (float, optional): Epsilon value for the attack. Defaults to 0.3.
+        num_iter (int, optional): Number of iterations for the attack. Defaults to 40.
+        alpha (float, optional): Alpha value for the attack. Defaults to 0.01.
+
+    Examples:
+        
+                >>> advsecurenet attack pgd --model-name=resnet18 --trained-on=cifar10 --model-weights=resnet18_cifar10_weights.pth
+                or
+                >>> advsecurenet attack pgd --config=pgd_attack_config.yml
+
+    Notes:
+            
+                If a configuration file is provided, matching CLI arguments will override the configuration file. The CLI arguments have priority.
+                Configuration file attributes must match the CLI arguments. For example, if the configuration file has a "model_name" attribute, the CLI argument must be named "model_name" as well.  
+    """
     return execute_general_attack(AttackType.PGD, config, attack_configs.PgdAttackConfig, **kwargs)
 
 
@@ -344,6 +451,38 @@ def pgd(config, **kwargs):
 @common_attack_options
 @click.option('--epsilon', default=None, type=click.FLOAT, help='Epsilon value for the attack.')
 def fgsm(config, **kwargs):
+    """
+    Command to execute a FGSM attack.
+
+    Args:
+
+        config (str, optional): Path to the attack configuration yml file.
+        model_name (str): The name of the model to be attacked.
+        trained_on (str): Dataset on which the model was trained.
+        model_weights (str): Path to model weights. If unspecified, uses the default path based on model_name and trained_on.
+        device (str, optional): Device for executing attacks. Defaults to CPU.
+        dataset_name (str, optional): Dataset for the attack. Choose "custom" for your own dataset.
+        custom_data_dir (str, optional): Path to custom dataset. Required if dataset_name is "custom".
+        dataset_part (str, optional): Which part of dataset to use for attack. Ignored if dataset_name is "custom".
+        random_samples (int, optional): Number of random samples for attack. Relevant only if dataset_part is "random" and dataset_name isn't "custom".
+        batch_size (int, optional): Batch size for attack execution. Defaults to 32.
+        verbose (bool, optional): Whether to print progress of the attack. Defaults to True.
+        save_result_images (bool, optional): Whether to save the adversarial images. Defaults to False.
+        result_images_dir (str, optional): Directory to save the adversarial images. Defaults to None.
+        epsilon (float, optional): Epsilon value for the attack. Defaults to 0.3.
+
+    Examples:
+        
+                >>> advsecurenet attack fgsm --epsilon 0.1
+                or
+                >>> advsecurenet attack fgsm --config=fgsm_attack_config.yml
+
+    Notes:
+                
+                    If a configuration file is provided, matching CLI arguments will override the configuration file. The CLI arguments have priority.
+                    Configuration file attributes must match the CLI arguments. For example, if the configuration file has a "model_name" attribute, the CLI argument must be named "model_name" as well.
+    
+    """
     return execute_general_attack(AttackType.FGSM, config, attack_configs.FgsmAttackConfig, **kwargs)
 
 @attack.command()
@@ -353,11 +492,49 @@ def fgsm(config, **kwargs):
 @click.option('--max-iterations', default=None, type=click.INT, help='Number of iterations for the attack.')
 @click.option('--deep-feature-layer', default=None, type=click.STRING, help='Deep feature layer for the attack.')
 @click.option('--learning-rate', default=None, type=click.FLOAT, help='Learning rate for the attack.')
-@click.option('--verbose', default=None, type=click.BOOL, help='Whether to print progress of the attack.')
 @click.option('--auto_generate_target_images', default=None, type=click.BOOL, help='Whether to automatically generate target images.')
 @click.option('--target_images_dir', default=None, type=click.STRING, help='Target images path.')
 @click.option('--maximum_generation_attempts', default=None, type=click.INT, help='Maximum number of attempts to generate target images.')
 def lots(config, **kwargs):
+    """
+    Command to execute a LOTS attack.
+
+    Args:
+
+        config (str, optional): Path to the attack configuration yml file.
+        model_name (str): The name of the model to be attacked.
+        trained_on (str): Dataset on which the model was trained.
+        model_weights (str): Path to model weights. If unspecified, uses the default path based on model_name and trained_on.
+        device (str, optional): Device for executing attacks. Defaults to CPU.
+        dataset_name (str, optional): Dataset for the attack. Choose "custom" for your own dataset.
+        custom_data_dir (str, optional): Path to custom dataset. Required if dataset_name is "custom".
+        dataset_part (str, optional): Which part of dataset to use for attack. Ignored if dataset_name is "custom".
+        random_samples (int, optional): Number of random samples for attack. Relevant only if dataset_part is "random" and dataset_name isn't "custom".
+        batch_size (int, optional): Batch size for attack execution. Defaults to 32.
+        verbose (bool, optional): Whether to print progress of the attack. Defaults to True.
+        save_result_images (bool, optional): Whether to save the adversarial images. Defaults to False.
+        result_images_dir (str, optional): Directory to save the adversarial images. Defaults to None.
+        mode (str, optional): Mode for the attack. Defaults to "LOTS".
+        epsilon (float, optional): Epsilon value for the attack. Defaults to 0.3.
+        max_iterations (int, optional): Number of iterations for the attack. Defaults to 40.
+        deep_feature_layer (str, optional): Deep feature layer for the attack. Defaults to "layer4".
+        learning_rate (float, optional): Learning rate for the attack. Defaults to 0.01.
+        auto_generate_target_images (bool, optional): Whether to automatically generate target images. Defaults to False.
+        target_images_dir (str, optional): Target images path. Defaults to None.
+        maximum_generation_attempts (int, optional): Maximum number of attempts to generate target images. Defaults to 100.
+
+    Examples:
+            
+                    >>> advsecurenet attack lots --model-name=resnet18 --trained-on=cifar10 --model-weights=resnet18_cifar10_weights.pth
+                    or
+                    >>> advsecurenet attack lots --config=lots_attack_config.yml
+
+    Notes:
+                        
+                            If a configuration file is provided, matching CLI arguments will override the configuration file. The CLI arguments have priority.
+                            Configuration file attributes must match the CLI arguments. For example, if the configuration file has a "model_name" attribute, the CLI argument must be named "model_name" as well.   
+        
+    """
     return execute_general_attack(AttackType.LOTS, config, attack_configs.LotsAttackConfig, **kwargs)
     
    
