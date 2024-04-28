801385236b73ce193b861dddc8a187b7b9a8e18e
Melih Catal <46859098+melihcatal@users.noreply.github.com>
2024-04-28 15:39:38 +0300

    model config types

--- advsecurenet/shared/types/model.py
+++ advsecurenet/shared/types/model.py
@@ -1,5 +1,14 @@
 from enum import Enum
 
+
 class ModelType(Enum):
+    """
+    Specifies the type of the model.
+    Supported types are:
+    - STANDARD: The model is a standard model from torchvision.models.
+    - CUSTOM: The model is a custom model provided by the package that is not present in torchvision.models. These models can be used for research purposes.
+    - EXTERNAL: The model is an external model that is not provided by the package. These models are loaded from external Python files.
+    """
     STANDARD = "standard"
     CUSTOM = "custom"
+    EXTERNAL = "external"
--- cli/types/model.py
+++ cli/types/model.py
@@ -3,13 +3,33 @@ from typing import List, Optional
 
 
 @dataclass
+class ModelNormConfig:
+    """ 
+    This dataclass is used to store the configuration of the normalization layer of a model.
+    """
+    add_norm_layer: bool
+    norm_mean: Optional[List[float]]
+    norm_std: Optional[List[float]]
+
+
+@dataclass
+class ModelPathConfig:
+    """
+    This dataclass is used to store the configuration of the paths of a model.
+    """
+    model_arch_path: Optional[str]
+    has_weights: bool
+    model_weights_path: Optional[str]
+
+
+@dataclass
 class ModelCliConfigType:
     """
     This dataclass is used to store the configuration of the model CLI.
     """
     model_name: str
+    is_external: bool
+    path_config: ModelPathConfig
     num_classes: int
     num_input_channels: int
-    add_norm_layer: bool
-    norm_mean: Optional[List[float]]
-    norm_std: Optional[List[float]]
+    norm_config: ModelNormConfig
