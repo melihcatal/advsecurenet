5919920913aed73fc00ef852d1a703577a9ee92f
Melih Catal <46859098+melihcatal@users.noreply.github.com>
2024-05-09 10:04:18 +0300

    improve config-default logging

--- cli/logic/config.py
+++ cli/logic/config.py
@@ -3,11 +3,12 @@ Logic for the 'configs' command.
 """
 
 import os
+from typing import Optional
 
 import click
 
-from advsecurenet.utils.config_utils import (generate_default_config_yaml,
-                                             get_available_configs)
+from cli.utils.config import (generate_default_config_yaml,
+                              get_available_configs)
 
 
 def cli_configs():
@@ -27,10 +28,28 @@ def cli_configs():
     click.echo("")
 
 
-def cli_config_default(config_name: str, save: bool, print_output: bool, output_path: str):
+def cli_config_default(config_name: str,
+                       save: Optional[bool] = False,
+                       print_output: Optional[bool] = False,
+                       output_path: Optional[str] = None
+                       ):
     """
     Save or print default configuration file.
+
+    Args:
+        config_name (str): The name of the configuration file.
+        save (bool): Whether to save the configuration file. Defaults to False.
+        print_output (bool): Whether to print the configuration file. Defaults to False.
+        output_path (str): The path to save the configuration file. Defaults to None.
+
+    Raises:
+        click.ClickException: If no configuration file name is provided.
     """
+
+    if not save and not print_output:
+        click.secho(
+            "Please provide either the --save or --print-output flag to save or print the configuration file!", fg="red")
+        return
 
     if config_name is None:
         raise click.ClickException("Please provide a configuration file name!")
@@ -40,7 +59,7 @@ def cli_config_default(config_name: str, save: bool, print_output: bool, output_
 
     try:
         default_config = generate_default_config_yaml(
-            config_name, output_path, save=save, config_subdir="cli")
+            config_name, output_path, save=save)
 
         if print_output:
             click.echo("*"*50)
@@ -53,7 +72,7 @@ def cli_config_default(config_name: str, save: bool, print_output: bool, output_
             click.echo(f"Generated default configuration file {config_name}!")
     except FileNotFoundError:
         click.echo(
-            f"Configuration file {config_name} not found! You can use the 'configs' command to list available configuration files.")
-    except Exception:
+            f"Configuration file {config_name} not found! You can use the 'configs' command to list available configuration files.", err=True)
+    except Exception as e:
         click.echo(
-            f"Error generating default configuration file {config_name}!", e)
+            f"Error generating default configuration file {config_name}! Details: {e}", err=True)
